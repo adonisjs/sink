fc9aa0cf3084121787a9b5d19d13c8cee59e7b7e
Harminder virk <virk.officials@gmail.com>
2019-09-04 14:21:55 +0530

    feat: add support for rendering markdown files

---
+++ test/render-markdown.spec.ts
@@ -0,0 +1,34 @@
+/*
+ * @adonisjs/sink
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+import test from 'japa'
+import { join } from 'path'
+import { Filesystem } from '@adonisjs/dev-utils'
+import { renderMarkdown } from '../src/renderMarkdown'
+
+const fs = new Filesystem(join(__dirname, '__app'))
+
+test.group('Render markdown', (group) => {
+  group.afterEach(async () => {
+    await fs.cleanup()
+  })
+
+  group.beforeEach(async () => {
+    await fs.ensureRoot()
+  })
+
+  /**
+   * Skipping the tests in CI, since there is no way to automatically
+   * test that everything is working fine without manual verification
+   */
+  test.skipInCI('render markdown file by opening it on', async () => {
+    await fs.add('foo.md', `## Hello world`)
+    await renderMarkdown(join(fs.basePath, 'foo.md'), '@adonisjs/core')
+  })
+})
